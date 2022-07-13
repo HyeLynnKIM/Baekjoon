636536544b89c951066ca186ac7ff098ba5e96de
메린 <64192139+HyeLynnKIM@users.noreply.github.com>
2022-07-13 11:20:56 +0900

    Update time complexity.py

--- time complexity.py
+++ time complexity.py
@@ -33,4 +33,3 @@ for i in range(20):
     print(i,"..",end=" ")
 print("\n속도 >>", sum(tt)/len(tt))
 #print("표준편차 : {}\tM : {}\tm : {}".format(round(numpy.std(tt),6),round(max(tt),6),round(min(tt),6)))
-#[출처] [파이썬]시간초과|작성자 형우 임
